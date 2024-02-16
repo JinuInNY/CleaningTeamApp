cb0b0a6de3990d75d959cb4f79aebb66d605048e
JinuInNY <sjinu329@gmail.com>
2024-02-16 00:04:46 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+import {getFontFamily} from './assets/fonts/inter/helper';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <title />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ components/Title/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const title = () => {
+  return;
+  <Text style={{fontSize: 50, style.fontFamily: getFontFamily('Inter', '500')}}>
+    Test
+  </Text>;
+};
+return title;
